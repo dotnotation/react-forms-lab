81287e9b4812b117cbca007e63ee17b0fdca5c92
dotnotation <dotinmotionproductions@gmail.com>
2022-02-01 14:41:59 -0500

    commit tree 99f4b5fe92e8c144a7302cad80963f8d2365a17f

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+    };
+  }
+
+  changeChars = event => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
+    let characters = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.changeChars} value={this.state.message}/>
+        <span>{characters}</span>
       </div>
     );
   }
